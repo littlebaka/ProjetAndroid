b631be0cd2988fe4d0d4de2d9cbbd06056de7c93
effyElena <Agathe@10.33.126.135>
2013-12-10 11:57:28 +0100

    Information spectacle et la liste

--- PuyDuFou/src/fr/exia/puydufou/activity/ShowActivity.java
+++ PuyDuFou/src/fr/exia/puydufou/activity/ShowActivity.java
@@ -1,9 +1,13 @@
 package fr.exia.puydufou.activity;
 
 import fr.exia.puydufou.R;
+import fr.exia.puydufou.asynctask.ShowAsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ShowActivity extends Activity {
 
@@ -11,6 +15,9 @@ public class ShowActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show);
+		ShowAsyncTask showAsyncTask = new ShowAsyncTask(this,(TextView) findViewById(R.id.ddcspecddc),(TextView) findViewById(R.id.actspecnb),(TextView) findViewById(R.id.dureenb),(TextView) findViewById(R.id.descspecdesc),(HorizontalScrollView) findViewById(R.id.horairespecdesc),(LinearLayout) findViewById(R.id.show_schedule_linearLayout) ,(TextView) findViewById(R.id.show_schedule_textView));
+		showAsyncTask.execute();
+		
 	}
 
 	@Override
