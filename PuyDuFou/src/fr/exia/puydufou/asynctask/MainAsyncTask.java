9f7a28768502b901d50ee36abf6f0285a83d229d
guiguiyoyo <guillaume.jolly@viacesi.fr>
2013-12-11 14:17:25 +0100

    vue shop !

--- PuyDuFou/src/fr/exia/puydufou/asynctask/MainAsyncTask.java
+++ PuyDuFou/src/fr/exia/puydufou/asynctask/MainAsyncTask.java
@@ -42,28 +42,32 @@ public class MainAsyncTask extends AsyncTask<Object, String, Integer> {
 		
 		if(isOnline()){
 			publishProgress("5");
-			this.datasLoadable.getAndStoreParcInformation(mainActivity.getContentResolver());
-			publishProgress("50");
-			this.datasLoadable.getAndStoreHoraireInformation(mainActivity.getContentResolver());
-			publishProgress("55");
-			this.datasLoadable.getAndStoreMenuInformation(mainActivity.getContentResolver());
-			publishProgress("60");
-			this.datasLoadable.getAndStoreServiceInformation(mainActivity.getContentResolver());
-			publishProgress("65");
-			this.datasLoadable.getAndStoreServiceMenuInformation(mainActivity.getContentResolver());
-			publishProgress("70");
-			this.datasLoadable.getAndStoreSpectacleInformation(mainActivity.getContentResolver());
-			publishProgress("75");
-			this.datasLoadable.getAndStoreSpectacleHoraireInformation(mainActivity.getContentResolver());
-			publishProgress("80");
-			this.datasLoadable.getAndStoreTypeServiceInformation(mainActivity.getContentResolver());
-			publishProgress("85");
-			this.datasLoadable.getAndStoreNoteServiceInformation(mainActivity.getContentResolver());
-			publishProgress("90");
-			this.datasLoadable.getAndStoreNoteSpectacleInformation(mainActivity.getContentResolver());
-			publishProgress("95");
-			this.datasLoadable.getAndStoreNoteInformation(mainActivity.getContentResolver());
-			publishProgress("100");
+			try{
+				this.datasLoadable.getAndStoreParcInformation(mainActivity.getContentResolver());
+				publishProgress("50");
+				this.datasLoadable.getAndStoreHoraireInformation(mainActivity.getContentResolver());
+				publishProgress("55");
+				this.datasLoadable.getAndStoreMenuInformation(mainActivity.getContentResolver());
+				publishProgress("60");
+				this.datasLoadable.getAndStoreServiceInformation(mainActivity.getContentResolver());
+				publishProgress("65");
+				this.datasLoadable.getAndStoreServiceMenuInformation(mainActivity.getContentResolver());
+				publishProgress("70");
+				this.datasLoadable.getAndStoreSpectacleInformation(mainActivity.getContentResolver());
+				publishProgress("75");
+				this.datasLoadable.getAndStoreSpectacleHoraireInformation(mainActivity.getContentResolver());
+				publishProgress("80");
+				this.datasLoadable.getAndStoreTypeServiceInformation(mainActivity.getContentResolver());
+				publishProgress("85");
+				this.datasLoadable.getAndStoreNoteServiceInformation(mainActivity.getContentResolver());
+				publishProgress("90");
+				this.datasLoadable.getAndStoreNoteSpectacleInformation(mainActivity.getContentResolver());
+				publishProgress("95");
+				this.datasLoadable.getAndStoreNoteInformation(mainActivity.getContentResolver());
+				publishProgress("100");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
 			
 		}else{
 			return 1;
